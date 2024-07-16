b7bed82a019bb878a67e835d15bcbe6a15110999
FujiyamaMasatoshi <fujiyama.masatoshi.130426@gmail.com>
2024-07-16 10:58:53 +0900

    update gitignore

--- Assets/Scripts/UI/UIEvent/RestaratEvent.cs
+++ Assets/Scripts/UI/UIEvent/RestaratEvent.cs
@@ -9,12 +9,6 @@ public class RestaratEvent : MonoBehaviour
     // restartイベント
     public void Restart()
     {
-        // ゲームマネージャーの情報を初期化する
-        GameManager.instance.InitGame();
-
-        // ゲーム時間を進める
-        Time.timeScale = 1.0f;
-
         // シーン遷移
         SceneManager.LoadScene("stage");
     }
